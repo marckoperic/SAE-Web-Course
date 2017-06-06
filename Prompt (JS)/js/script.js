4b93c72fd395422d1017f25bbbaadc3925de09b2
Marko Peric <marcko.peric@gmail.com>
2017-06-06 18:01:03 +0200

    second update

---
+++ Prompt (JS)/js/script.js
@@ -0,0 +1,11 @@
+var name = prompt("Your name?");
+var lastname = prompt("Your lastname?");
+var age = prompt("Your age?");
+
+var fullName = name + " " + lastname
+
+
+console.log("Your name is " + fullName)
+console.log("You have " + age + " years")
+
+alert("Hello " + fullName + " You are Welcome!" );
